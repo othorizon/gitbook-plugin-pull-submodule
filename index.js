1d72ba086002fde0fe2e576e98af0af62c35d10e
othorizon <othorizon@users.noreply.github.com>
2018-05-03 19:12:36 +0800

    update for test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
             const root = this.resolve('')
             console.log("start pull submodule");
             //直接调用命令 git submodule init;git submodule update
-            process.execSync("ls -a",
+            process.exec("ls -a",
                 function (error, stdout, stderr) {
                     console.log(stdout);
                     if (error !== null) {
